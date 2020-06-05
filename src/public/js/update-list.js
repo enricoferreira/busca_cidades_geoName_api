8485ec0b7172be87b3821ab129fdc6dc39bdec05
Enrico Augusto Rocha Ferreira <enricoferreira41@gmail.com>
2020-06-05 20:23:06 -0300

    Inserindo Sigla do paísna List

--- src/public/js/update-list.js
+++ src/public/js/update-list.js
@@ -2,7 +2,7 @@
 export const insertList = (cities) =>`
         <div class="list-group shadow h-50 overflow-auto">
             ${cities.geonames.length > 0 ? cities.geonames.map(city => `
-            <a href="#" data-toggle="modal" data-target="#modal-detail-city" class="list-group-item list-group-item-action pointer" data-city-id="${city.geonameId}">${city.toponymName}</a>
+            <a href="#" data-toggle="modal" data-target="#modal-detail-city" class="list-group-item list-group-item-action pointer" data-city-id="${city.geonameId}">${city.toponymName}, ${city.countryCode}</a>
             `).join(' '): 
             `
             <div class="alert alert-danger" role="alert">
